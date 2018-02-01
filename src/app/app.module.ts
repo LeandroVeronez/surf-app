092951eb2d347e3cb976a56bb4d8d0a438f4a079
Pedro Castro <pedrocastro94@gmail.com>
2018-02-01 14:23:50 -0200

    Organizacao da tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { PraiasPageModule } from '../pages/praias/praias.module';
+import { MapasPageModule } from '../pages/mapas/mapas.module';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
     ContactPage,
     HomePage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    PraiasPageModule,
+    MapasPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
     ContactPage,
     HomePage,
     TabsPage
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
+
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { PraiasPage } from '../praias/praias';
+import { MapasPage } from '../mapas/mapas';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,8 +12,8 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab2Root = PraiasPage;
+  tab3Root = MapasPage;
 
   constructor() {
 
